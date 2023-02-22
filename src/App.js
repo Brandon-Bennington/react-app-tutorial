4a71bb78ce2a53dc83a8466bd53c73d0ad8cb9d9
brandon-bennington <brandon.bennington@codeimmersives.com>
2023-02-22 15:25:16 -0300

    All Working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table'
+import Form from './Form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//class-based component called App
+//extends inherits from React.Component
+// tells App to behave like a component.
 
-export default App;
+class App extends Component {
+  //set initial state of component
+    state = {
+      characters: [{
+          name: 'Charlie',
+          job: 'Janitor',
+        },
+        {
+          name: 'Mac',
+          job: 'Bouncer',
+        },
+        {
+          name: 'Dee',
+          job: 'Aspring actress',
+        },
+        {
+          name: 'Dennis',
+          job: 'Bartender',
+        },
+      ]
+    }
+    
+  //create simple method here to remove character
+  removeCharacter = (index) => {
+    //now that we've defined this.state, we ca use
+    const { characters } = this.state
+    //we can use setState to update the state
+    this.setState({
+      // remove character at passed in index by returning
+      // a new list excluding that character
+      characters: characters.filter((_, i) => {
+        return i !== index
+      }),
+    })
+  
+  }
+      // // create method to add a character 
+    // addCharacter = (name_, job) => {
+
+    //   this.characters.push({name: name_, });
+    //   this.setState({
+    //     characters: this.characters
+    //   });
+    // }
+
+    //we add the handle submit here , because 
+    //the characters are in here 
+    // NOTE ON SYNTAX: passing a character to addCharacter 
+    // using (...) spread operator to unpack characters array and adding
+    // a new character
+  
+  addCharacter = character => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
+
+  render() {
+    // make sure return only returns one html element!  
+    // we are passing the characters from state
+    // and the removeCharacter function that we wrote
+    // so table can use it later on 
+    return (
+      <div className="container"> 
+        <Table 
+          characterData={this.state.characters} 
+          removeCharacter={this.removeCharacter} 
+        /> 
+        <Form addCharacter={this.addCharacter}/>
+      </div>
+    )
+  }
+  }
+// make it accessible to the rest of your application
+export default App
